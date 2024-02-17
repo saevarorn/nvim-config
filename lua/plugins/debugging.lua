917545000adb6027141f820d2a59c822ec047d6b
Sævar Örn Kjartansson <saevar@varist.com>
2024-02-17 13:25:26 +0000

    Setup debugging

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      -- "theHamsta/nvim-dap-virtual-text",
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+      -- "Pocco81/DAPInstall.nvim",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+      require("dap-go").setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    end,
+  },
+}
